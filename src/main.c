abca2e2451df68cb47a922d3deed326c55c0a71d
root <root@localhost>
2024-11-01 03:34:13 +0300

    auto publish 1730421252899

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main () {
+    FILE *file = fopen("data.txt", "w");
+    if (file == NULL) {
+        printf("Error opening file");
+        exit(1);
+    }
+
+    char sentence[100];
+    printf("Enter a sentence (up to 100 characters): ");
+    fgets(sentence, sizeof(sentence), stdin);
+
+    fprintf(file, "%s", sentence);
+    fclose(file);
+    return 0;
+}
